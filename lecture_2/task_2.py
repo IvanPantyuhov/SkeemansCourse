e06af8fb9f001219621acb8f6cda27d3f03e2a2e
IvanPantyuhov <pantyuhovr@gmail.com>
2021-09-14 03:31:46 +0300

    1

---
+++ lecture_2/task_2.py
@@ -0,0 +1,6 @@
+number = int(input("Enter the number of rows - "))
+for a in range(number, 0, -1):
+    for b in range(1, a + 1):
+        print(b, end=" ")
+    print("\r")
+
---
+++ lecture_2/task_3.py
@@ -0,0 +1,3 @@
+word = str(input("Write the palidrome word - "))
+reversed=''.join(reversed(word))
+print(reversed)

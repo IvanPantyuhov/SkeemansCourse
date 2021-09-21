4b591f3b63dca51e07dc2a169600ba196e737d9c
IvanPantyuhov <pantyuhovr@gmail.com>
2021-09-21 16:32:16 +0300

    Bus

--- lecture_3/task2.py
+++ lecture_3/task2.py
@@ -1,16 +1,24 @@
 class Vehicle:
-    def __init__(self, brand, year, max_speed):
+    def __init__(self, brand, year_of_production, max_speed):
         self.brand = brand
-        self.year = year
+        self.year_of_production = year_of_production
         self.max_speed = max_speed
 
     def move(self):
-        print(f'Vehicle {self.brand} is moving with max speed {self.max_speed}!')
-
-    def set_max_speed(self, max_speed):
-        self.max_speed = max_speed
+        print(f'{self.brand} is moving with max speed {self.max_speed}')
 
 class Bus(Vehicle):
+    def __init__(self):
+        super().__init__()
 
     def move(self):
-        return f'Bus {self.brand} is moving '
+        print(f'Bus {self.brand} is moving with max speed {self.max_speed}')
+
+if __name__ == '__main__':
+    mercedes = Vehicle("Mercedes-Benz Tourismo", 2021, 150)
+    isuzu = Vehicle("Isuzu Ataman", 2019, 170)
+    setra = Vehicle("Setra TopClass", 2020, 180)
+
+    mercedes.move()
+    isuzu.move()
+    setra.move()

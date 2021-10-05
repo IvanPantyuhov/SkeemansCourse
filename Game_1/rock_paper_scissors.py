82e0e00aefe143259b0aa11fe383c33d9faaa430
IvanPantyuhov <pantyuhovr@gmail.com>
2021-10-05 16:18:47 +0300

    Rock, paper, scissors

---
+++ Game_1/rock_paper_scissors.py
@@ -0,0 +1,35 @@
+from random import choice
+
+values = ['rock', 'paper', 'scissors']
+print('Welcome to the games "ROCK, PAPER & SCISSORS"')
+
+while True:
+    user = input('Make a choice "Rock, Paper, Scissors": ').lower()
+    computer = choice(values)
+    print(f"\nYou chose {user.upper()}, computer chose {computer.upper()}.\n")
+
+    if user == computer:
+        print("Tie, try again")
+        continue
+    elif user == "rock":
+        if computer == "scissors":
+            print("Rock smashes scissors! You win.\n")
+        else:
+            print("Paper covers rock! You lose.\n")
+    elif user == "paper":
+        if computer == "rock":
+            print("Paper covers the rock! You win.\n")
+        else:
+            print("Scissors cuts paper! You lose.\n")
+    elif user == "scissors":
+        if computer == "paper":
+            print("Scissors cut paper! You win.\n")
+        else:
+            print("Rock smashes scissors! You lose.\n")
+
+    play_again = input("Would you like to play again? (yes/no): ").lower()
+    if play_again == "yes":
+        continue
+    else:
+        print('See you next time')
+        break
